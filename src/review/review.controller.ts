00db5b0649a49d373c01845fd33e7dd06d8bb30c
Andrey <geliosal39hon@gmail.com>
2023-09-25 14:43:59 +0300

    add telegram_bot

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -16,15 +16,31 @@ import { ReviewService } from './review.service';
 import { REVIEW_NOT_FOUND } from './review.constants';
 import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 import { IdValidationPipe } from '../pipes/ad-validation.pipe';
+import { TelegramService } from 'src/telegram/telegram.service';
 
 @Controller('review')
 export class ReviewController {
-	constructor(private readonly reviewSevice: ReviewService) {}
+	constructor(
+		private readonly reviewSevice: ReviewService,
+		private readonly telegramService: TelegramService,
+	) {}
 
 	@UsePipes(new ValidationPipe())
 	@Post('create')
 	async create(@Body() dto: CreateReviewDto) {
 		return this.reviewSevice.create(dto);
+	}
+
+	@UsePipes(new ValidationPipe())
+	@Post('notify')
+	async notify(@Body() dto: CreateReviewDto) {
+		const message =
+			`Имя: ${dto.name}\n` +
+			`Заголовок: ${dto.title}\n` +
+			`Описание: ${dto.description}\n` +
+			`Рейтинг: ${dto.rating}\n` +
+			`ID Продукта: ${dto.productId}\n`;
+		return this.telegramService.sendMessage(message);
 	}
 
 	@UseGuards(JwtAuthGuard)
